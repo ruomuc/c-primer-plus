1a9c8621744339d7a25659d4db4d8b3f71ab029a
ruomu <252615299@qq.com>
2022-09-04 19:56:21 +0800

    ch11复习题

---
+++ ch11/复习题/11.1.cpp
@@ -0,0 +1,8 @@
+// // prototype
+// Stonewt operator*(double mult);
+
+// // definition
+// Stonewt Stonewt::operator*(double mult)
+// {
+//   return Stonewt(mult * pounds);
+// }
---
+++ ch11/复习题/11.4.cpp
@@ -0,0 +1,10 @@
+// // prototype
+
+// friend Stonewt operator*(double mult, const Stonewt &s);
+
+// // definition
+
+// Stonewt operator*(double mult, const Stonewt &s)
+// {
+//   return Stonewt(mult * s.pounds);
+// }
